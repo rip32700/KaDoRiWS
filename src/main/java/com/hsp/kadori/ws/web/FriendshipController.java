f5dde1db7b6762ef3aa84ed336e719e2c7866506
Beli90 <andi.blubb@hotmail.de>
2016-09-25 21:41:42 +0200

    Added FriendshipController for adding friends to db

---
+++ src/main/java/com/hsp/kadori/ws/web/FriendshipController.java
@@ -0,0 +1,26 @@
+package com.hsp.kadori.ws.web;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hsp.kadori.ws.dao.FriendshipDAO;
+import com.hsp.kadori.ws.domain.Friendship;
+
+@RestController
+@RequestMapping("friendship")
+public class FriendshipController {
+	@Inject
+	FriendshipDAO repository;
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public ResponseEntity<?> createFriendship(@RequestBody Friendship friendship) {
+		friendship = repository.save(friendship);
+	    return new ResponseEntity<>(null, HttpStatus.CREATED);
+	}
+}
